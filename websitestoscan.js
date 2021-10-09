ddde98b5deef5875e313d855ba5853ae723ef7d8
barikanwar <barik.anwar@gmail.com>
2021-10-09 22:22:36 +0530

    updating file with temporary website example

--- websitestoscan.js
+++ websitestoscan.js
@@ -1,14 +1,7 @@
 // Add websites here for which you want to check the broken link. 
 const websites=[
-/*'http://www.3monoamines.com/',
-'https://www.samsca.com/',
-'https://www.jynarque.com/',
-'https://www.jynarquehcp.com/',
-'https://www.pkdinfo.com/',
-'https://jnqbrand.otsuka.acsitefactory.com/',
-'https://www.adpkdpeermentorprogram.com',
-'http://social.jynarque.com/'*/
-'http://www.bigkidneybigproblem.com/'
+'https://www.google.com/',
+'https://www.facebook.com'
 ]
 
 module.exports.websites=websites;
